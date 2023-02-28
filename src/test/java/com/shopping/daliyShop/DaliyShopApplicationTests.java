d69a6339d786f8d8440a88d8c0f7e6af7aa1d732
DESKTOP-4SJU0C0\LEEJH <zndn121@gmail.com>
2023-02-28 16:46:05 +0900

    Initial commit : mysql 연동, template(header, footer, loginModal 분리)

---
+++ src/test/java/com/shopping/daliyShop/DaliyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping.daliyShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaliyShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
