a5060d5c2ca08dcb0474f026bcc0a8d68150cbef
jinhyeong.lee <zndn121@gmail.com>
2023-03-30 23:53:50 +0900

    Test시 system.out -> Assertions.assertThat으로 수정 및 intellij compile오류 수정(@ComponentScan)

--- src/main/java/com/shopping/daliyShop/mapper/TestMapper.java
+++ src/main/java/com/shopping/daliyShop/mapper/TestMapper.java
@@ -2,8 +2,10 @@ package com.shopping.daliyShop.mapper;
 
 import com.shopping.daliyShop.model.TestModel;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.context.annotation.ComponentScan;
 
 @Mapper
+@ComponentScan
 public interface TestMapper {
     TestModel selectByIdTest(String id);
 }
--- src/test/java/com/shopping/daliyShop/MyBatisTest.java
+++ src/test/java/com/shopping/daliyShop/MyBatisTest.java
@@ -2,6 +2,7 @@ package com.shopping.daliyShop;
 
 import com.shopping.daliyShop.mapper.TestMapper;
 import com.shopping.daliyShop.model.TestModel;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,7 +17,13 @@ public class MyBatisTest {
 
     @Test
     public void selectByIdTest(){
+        // mybatis를 통해 db에서 가져온 데이터
         TestModel testModel = testMapper.selectByIdTest("1");
-        System.out.println("testModel = " + testModel.toString());
+        // TestModel 객체에 동일한 데이터를 넣어서 비교
+        TestModel testModel1 = new TestModel();
+        testModel1.setId("1");
+        testModel1.setPw("1");
+        testModel1.setName("테스트");
+        Assertions.assertThat(testModel).isEqualTo(testModel1);
     }
 }
