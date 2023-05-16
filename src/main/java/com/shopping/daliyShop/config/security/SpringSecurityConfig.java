1db9250cd67f9e703f9821b85fdc6a3fbb3071c4
jinhyeong.lee <zndn121@gmail.com>
2023-05-16 20:48:46 +0900

    로그인 여부 파악 기능 추가(Thymeleaf를 이용한 Spring Security)

--- src/main/java/com/shopping/daliyShop/config/security/SpringSecurityConfig.java
+++ src/main/java/com/shopping/daliyShop/config/security/SpringSecurityConfig.java
@@ -1,13 +1,21 @@
 package com.shopping.daliyShop.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -30,21 +38,37 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/" ,"/index", "/loginTest","/test/**","/thymeleafTest").permitAll() /* 인증이 필요하지 않은 경로를 예외처리함 (추가로 넣기 위해서는 ,로 구분해 주거나 *를 사용하면 됨) */
-                .anyRequest().authenticated() /* 어떠한 URL로 접근하던지 인증이 필요함을 설정 */
+                .antMatchers("/order").authenticated() /* 인증이 필요하지 않은 경로를 예외처리함 (추가로 넣기 위해서는 ,로 구분해 주거나 *를 사용하면 됨) */
+                .anyRequest().permitAll() /* 어떠한 URL로 접근하던지 인증이 필요함을 설정 */
             .and()
                 .exceptionHandling() /* 예외처리를 핸들링하기 위한 핸들러 사용 */
                     .authenticationEntryPoint(entryPointUnauthorizedHandler) /* 인증 인가 예외처리 핸들러 */
             .and()
                 .formLogin() /* FORM 방식 로그인을 사용할 것임을 알림 */
-                .loginPage("/loginTest") /* 로그인 페이지가 따로 없고, 모달을 띄우는 방식이므로 사용하지 않음 */
+                .loginPage("/loginTest")
                 .loginProcessingUrl("/loginProc") /* 로그인 form의 action */
-                .usernameParameter("usrId") /* 로그인 form의 ID의 name */
-                .passwordParameter("usrPw") /* 로그인 form의 PW의 name */
-                .defaultSuccessUrl("/index", true) /* 로그인 성공시 이동할 URL을 적음 */
+                .usernameParameter("userId") /* 로그인 form의 ID의 name */
+                .passwordParameter("userPw") /* 로그인 form의 PW의 name */
+//                .defaultSuccessUrl("/index", true) /* 로그인 성공시 이동할 URL을 적음 */
                 .permitAll()
             .and()
                 .logout() /* 로그아웃 기능 추가 */
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/index")
+                .addLogoutHandler(new LogoutHandler() { //logout이 성공했을 때 처리할 내용
+                    @Override
+                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                        HttpSession session = request.getSession();
+                        session.invalidate();
+                    }
+                })
+                .logoutSuccessHandler(new LogoutSuccessHandler() { //logout 시 이동할 url 및 더 많은 로직 구현 가능
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        response.sendRedirect("/index"); //로그아웃 시 로그인 할 수 있는 페이지로 이동하도록 처리한다.
+                    }
+                })
+                .deleteCookies("remember-me")
             .and()
                 .csrf().disable(); /* csrf 토큰 비활성화 */
     }
