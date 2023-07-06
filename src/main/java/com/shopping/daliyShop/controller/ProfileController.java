234e8cb662cf395949bc76e0a3867bce55f62b40
ShinYoungRyul <tlsdudfuf124@naver.com>
2023-07-06 19:50:30 +0900

    회원가입 #1

--- src/main/java/com/shopping/daliyShop/controller/ProfileController.java
+++ src/main/java/com/shopping/daliyShop/controller/ProfileController.java
@@ -1,18 +1,53 @@
 package com.shopping.daliyShop.controller;
 
+import com.shopping.daliyShop.service.ProfileService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 
 @Controller
 public class ProfileController {
-    @RequestMapping(value = "/account")
+
+    private final ProfileService profileService;
+
+    public ProfileController(ProfileService profileService) {
+        this.profileService = profileService;
+    }
+
+
+    @RequestMapping(value = "/account", method = RequestMethod.GET)
     public String account(Model model) throws Exception{
-        System.out.println("HomeController.account");
+        System.out.println("ProfileController.account");
         return "account";
     }
 
+    @RequestMapping(value = "/join", method = RequestMethod.POST)
+    @ResponseBody
+    public String join(@RequestBody HashMap<String, Object> map) throws Exception{
+        System.out.println("ProfileController.join");
+        System.out.println("map : " + map);
+        profileService.join(map);
+        return "/loginTest";
+    }
+
+    @RequestMapping(value = "/idCheck", method = RequestMethod.GET)
+    @ResponseBody
+    public String idCheck(HttpServletRequest request) throws Exception{
+        System.out.println("ProfileController.idCheck");
+        String usrId = request.getParameter("usrId");
+        int result = profileService.idCheck(usrId);
+
+        return Integer.toString(result);
+    }
+
     @RequestMapping(value = "/myInfo")
+
     public String myInfo(Model model) throws Exception{
         System.out.println("HomeController.my-info");
         return "myInfo";
--- src/main/java/com/shopping/daliyShop/mapper/ProfileMapper.java
+++ src/main/java/com/shopping/daliyShop/mapper/ProfileMapper.java
@@ -2,6 +2,14 @@ package com.shopping.daliyShop.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.HashMap;
+
 @Mapper
 public interface ProfileMapper {
+
+    int idCheck(String usrId);
+    int join(HashMap<String, Object> map);
+    int joinAuth(HashMap<String, Object> map);
+
+
 }
